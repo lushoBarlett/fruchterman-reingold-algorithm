672b7e6c38cda2bc4aac3810d4a57c6c813a75a7
Luciano Barletta <lushobarlett@gmail.com>
2021-11-06 12:31:05 -0300

    fix bugs and add edges

--- graph.py
+++ graph.py
@@ -1,8 +1,11 @@
+
 Vertex : type = str
+Edge : type = tuple[Vertex, Vertex]
 AdjacencyList : type = dict[Vertex, set[Vertex]]
 
+
 class Graph:
-    def __init__(self):
+    def __init__(self) -> None:
         self.adjacency_list : AdjacencyList = {}
     
     def define_vertex(self, name : str) -> None:
@@ -18,26 +21,33 @@ class Graph:
         if b not in self.adjacency_list:
             raise ValueError(f"Vertex '{b}' is not defined")
 
-        self.adjacency_list[a].push(b);
-        self.adjacency_list[b].push(a);
+        self.adjacency_list[a].append(b);
+        self.adjacency_list[b].append(a);
     
     def vertices(self) -> set[Vertex]:
         return set(self.adjacency_list.keys())
 
+    def edges(self) -> list[Edge]:
+        edges : list[Edge] = []
+        for a in self.adjacency_list:
+            for b in self.adjacency_list[a]:
+                edges.append((a, b))
+        return edges
+
+
 def from_file(filename : str) -> Graph:
     graph : Graph = Graph()
     
-    with open(filename, "w") as file:
-
-        vertices : int = int(file.readline())
+    with open(filename, "r") as file:
+        vertices : int = int(file.readline().strip())
         while vertices:
-            graph.define_vertex(file.readline())
+            graph.define_vertex(file.readline().strip())
             vertices -= 1
         
-        line : str = file.readline()
+        line : str = file.readline().strip()
         while line != "":
             (v1, _, v2) = line.partition(" ")
             graph.define_edge(v1, v2)
-            line = file.readline()
+            line = file.readline().strip()
     
     return graph
